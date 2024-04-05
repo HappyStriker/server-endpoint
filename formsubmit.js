055721e0c14bfb01952a6b0ced6e12fa9c110deb
Daniel Messner <daniel@messner.top>
2024-04-05 13:31:17 +0200

    Simplify object creation from form data.

--- formsubmit.js
+++ formsubmit.js
@@ -7,12 +7,7 @@ const buttonElement = document.getElementById('button');
 formElement.addEventListener('submit', async (event) => {
   event.preventDefault();
 
-  const formData = new FormData(event.target);
-
-  let formObject = {};
-  formData.forEach((value, key) => {
-    formObject[key] = value;
-  });
+  const formData = Object.fromEntries(new FormData(event.target));
 
   try {
     const response = await fetch(`http://localhost:8080/api/v1/formsubmit`, {
